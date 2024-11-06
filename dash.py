83e3050edbe1a4c278c88f01ea5ff139b58d61e5
ViniciusJS <163647405+Vinicius380@users.noreply.github.com>
2024-11-06 16:53:19 -0300

    Dash

--- dash.py
+++ dash.py
@@ -6,9 +6,7 @@ from query import *
 from datetime import datetime
 from streamlit_modal import Modal
 
-
 query = "SELECT * FROM tb_dados"    # Consulta com o banco de dados.
-
 df = conexao(query)                 # Carregar os dados do MySQL.
 
 df['tempo_registro'] = pd.to_datetime(df['tempo_registro'])  # Converter para datetime
@@ -19,126 +17,100 @@ if st.button("Atualizar dados"):     # Botão para atualização dos dados.
 # MENU LATERAL
 st.sidebar.header('Selecione a informação para gerar o gráfico')
 
-# Seleção de colunas X      - SELECTBOX > Cria uma caixa de seleção na barra lateral
+# Seleção de colunas X
 colunaX = st.sidebar.selectbox(
     'Eixo X',
     options=['tempo_registro', 'oximetro_saturacao_oxigenio', 'oximetro_frequencia_pulso', 'frequencia_cardiaca', 'temperatura'],
     index=0
 )
 
+# Seleção de colunas Y
 colunaY = st.sidebar.selectbox(
-    'Eixo X',
+    'Eixo Y',
     options=['tempo_registro', 'oximetro_saturacao_oxigenio', 'oximetro_frequencia_pulso', 'frequencia_cardiaca', 'temperatura'],
     index=1
 )
 
-# Verificar quais os atributos do filtro
+# Verificar se a coluna selecionada está nos eixos X ou Y
 def filtros(atributo):
     return atributo in [colunaX, colunaY]
 
 # Filtro de Range -> SLIDER
 st.sidebar.header('Selecione o Filtro')
 
-
-# UMIDADE
+# Oximetro Saturação Oxigênio
 if filtros('oximetro_saturacao_oxigenio'):
-    umidade_range = st.sidebar.slider(
+    oxigenio_range = st.sidebar.slider(
         'Oximetro Saturação Oxigênio',
-        # Valor Mínimo
         min_value=float(df['oximetro_saturacao_oxigenio'].min()),
-        # Valor Máximo
         max_value=float(df['oximetro_saturacao_oxigenio'].max()),
-        # Faixa de valores selecionados
         value=(float(df['oximetro_saturacao_oxigenio'].min()), float(df['oximetro_saturacao_oxigenio'].max())),
-        # Incremento para cada movimento do slider
         step=0.1
     )
 
-
-# TEMPERATURA
+# Frequência Pulso
 if filtros('oximetro_frequencia_pulso'):
-    oximetro_frequencia_pulso = st.sidebar.slider(
+    pulso_range = st.sidebar.slider(
         'Frequencia Pulso',
-        # Valor Mínimo
         min_value=float(df['oximetro_frequencia_pulso'].min()),
-        # Valor Máximo
         max_value=float(df['oximetro_frequencia_pulso'].max()),
-        # Faixa de valores selecionados
         value=(float(df['oximetro_frequencia_pulso'].min()), float(df['oximetro_frequencia_pulso'].max())),
-        # Incremento para cada movimento do slider
         step=0.1
     )
 
-
-# PRESSAO
+# Frequência Cardíaca
 if filtros('frequencia_cardiaca'):
     pressao_range = st.sidebar.slider(
         'Frequência Cardiaca',
-        # Valor Mínimo
         min_value=float(df['frequencia_cardiaca'].min()),
-        # Valor Máximo
         max_value=float(df['frequencia_cardiaca'].max()),
-        # Faixa de valores selecionados
         value=(float(df['frequencia_cardiaca'].min()), float(df['frequencia_cardiaca'].max())),
-        # Incremento para cada movimento do slider
         step=0.1
     )
 
-
-# ALTITUDE
+# Temperatura
 if filtros('temperatura'):
     altitude_range = st.sidebar.slider(
         'Temperatura',
-        # Valor Mínimo
         min_value=float(df['temperatura'].min()),
-        # Valor Máximo
         max_value=float(df['temperatura'].max()),
-        # Faixa de valores selecionados
         value=(float(df['temperatura'].min()), float(df['temperatura'].max())),
-        # Incremento para cada movimento do slider
         step=0.1
     )
-
 
 # Cria uma cópia do df original
 df_selecionado = df.copy()
 
+# Aplicando os filtros de acordo com os ranges selecionados
 if filtros('oximetro_saturacao_oxigenio'):
-    df_selecionado - df_selecionado[df_selecionado
-    (df_selecionado['oximetro_saturacao_oxigenio'] >= oximetro_frequencia_pulso[0]) &
-    (df_selecionado['oximetro_saturacao_oxigenio'] <= oximetro_frequencia_pulso[1])
+    df_selecionado = df_selecionado[
+        (df_selecionado['oximetro_saturacao_oxigenio'] >= oxigenio_range[0]) &
+        (df_selecionado['oximetro_saturacao_oxigenio'] <= oxigenio_range[1])
     ]
 
 if filtros('oximetro_frequencia_pulso'):
-    df_selecionado - df_selecionado[df_selecionado
-    (df_selecionado['oximetro_frequencia_pulso'] >= oximetro_frequencia_pulso[0]) &
-    (df_selecionado['oximetro_frequencia_pulso'] <= oximetro_frequencia_pulso[1])
+    df_selecionado = df_selecionado[
+        (df_selecionado['oximetro_frequencia_pulso'] >= pulso_range[0]) &
+        (df_selecionado['oximetro_frequencia_pulso'] <= pulso_range[1])
     ]
 
 if filtros('frequencia_cardiaca'):
-    df_selecionado - df_selecionado[df_selecionado
-    (df_selecionado['frequencia_cardiaca'] >= oximetro_frequencia_pulso[0]) &
-    (df_selecionado['frequencia_cardiaca'] <= oximetro_frequencia_pulso[1])
+    df_selecionado = df_selecionado[
+        (df_selecionado['frequencia_cardiaca'] >= pressao_range[0]) &
+        (df_selecionado['frequencia_cardiaca'] <= pressao_range[1])
     ]
 
 if filtros('temperatura'):
-    df_selecionado - df_selecionado[df_selecionado
-    (df_selecionado['temperatura'] >= oximetro_frequencia_pulso[0]) &
-    (df_selecionado['temperatura'] <= oximetro_frequencia_pulso[1])
+    df_selecionado = df_selecionado[
+        (df_selecionado['temperatura'] >= altitude_range[0]) &
+        (df_selecionado['temperatura'] <= altitude_range[1])
     ]
 
-if filtros('tempo_registro'):
-    df_selecionado - df_selecionado[df_selecionado
-    (df_selecionado['tempo_registro'] >= oximetro_frequencia_pulso[0]) &
-    (df_selecionado['tempo_registro'] <= oximetro_frequencia_pulso[1])
-    ]
-
-
-# GRAFICOS
+# Exibir Tabela
 def Home():
     with st.expander('Tabela'):
         mostrarDados = st.multiselect(
-            'Filtro: ',
+            'Filtros: ',
             df_selecionado.columns,
             default=[],
             key='showData_home'
@@ -148,51 +120,50 @@ def Home():
             st.write(df_selecionado[mostrarDados])
 
     if not df_selecionado.empty:
-        media_cardiaca = df_selecionado['frequencia_cardiaca'].mean(),
+        media_cardiaca = df_selecionado['frequencia_cardiaca'].mean()
+        media_pulso = df_selecionado['oximetro_frequencia_pulso'].mean()
         media_oxigenio = df_selecionado['oximetro_saturacao_oxigenio'].mean()
 
         media1, media2 = st.columns(2, gap='large')
 
         with media1:
-            st.info('Média de Registros de Frequência Cardíaca', icon='📍')
+            st.info('Média de Frequência Cardíaca', icon='📍')
             st.metric(label='Média', value=f'{media_cardiaca:.2f}')
 
         with media2:
-            st.info('Média de Registros de Oxigênio', icon='📍')
+            st.info('Média de Saturação de Oxigênio', icon='📍')
             st.metric(label='Média', value=f'{media_oxigenio:.2f}')
 
         st.markdown('''-----------''')
 
-
-# GRÁFICOS
+# Geração de Gráficos
 def graficos():
     st.title('Dashboard Monitoramento')
 
-    aba1 = st.table(['Gráfico de Linha', 'Gráfico de Dispersão'])
+    if df_selecionado.empty:
+        st.write('Nenhum dado disponível para gerar o gráfico.')
+        return
 
-    with aba1:
-        if df_selecionado.empty:
-            st.write('Nenhum dado está disponível para gerar o gráfico')
-            return
-        
-        if colunaX == colunaY:
-            st.warning('Selecione uma opção diferente para os eixos X e Y')
-            return
-        
-        try:
-            grupo_dados1 = df_selecionado.groupby(by=[colunaX]).size().reset_index(name='contagem')
+    if colunaX == colunaY:
+        st.warning('Selecione colunas diferentes para os eixos X e Y')
+        return
 
-            fig_valores = px.bar(
-                grupo_dados1,
-                x=colunaX,
-                y='Contagem',
-                orientation='h',
-                title=f'Contagem de Registros por {colunaX.capitalize()}',
-                color_discrete_sequence=['#0083b8'],
-                template='plotly_white'
-            )
+    try:
+        grupo_dados = df_selecionado.groupby(by=[colunaX]).size().reset_index(name='Contagem')
 
-        except Exception as e:
-            st.error(f'Erro ao criar o gráfico de linha: {e}')
-
+        fig_valores = px.bar(
+            grupo_dados,
+            x=colunaX,
+            y='Contagem',
+            orientation='h',
+            title=f'Contagem de Registros por {colunaX.capitalize()}',
+            color_discrete_sequence=['#0083b8'],
+            template='plotly_white'
+        )
         st.plotly_chart(fig_valores, use_container_width=True)
+
+    except Exception as e:
+        st.error(f'Erro ao criar o gráfico: {e}')
+
+# Exibir as funções
+Home()
